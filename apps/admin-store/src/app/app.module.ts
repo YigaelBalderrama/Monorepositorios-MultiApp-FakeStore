3f556eda1f4f6ce75830a432a2378474d3d967e3
YigaelAlejandro <yigaelzero@gmail.com>
2023-02-08 16:30:20 -0400

    header added

--- apps/admin-store/src/app/app.module.ts
+++ apps/admin-store/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BarComponent, StoreuiModule } from "@fake-store/storeui";
+import { StoreuiModule } from '@fake-store/storeui';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { appRoutes } from './app.routes';
     BrowserModule,
     StoreuiModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    NgbModule,
   ],
   exports: [
     
--- apps/client-store/src/app/app.module.ts
+++ apps/client-store/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { StoreuiModule } from '@fake-store/storeui';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    StoreuiModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
